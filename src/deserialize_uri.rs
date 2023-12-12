d140a5c0a22631bdd513073581609584a0eb856e
MarcusGrass <marcus.grass@protonmail.com>
2023-12-12 16:06:39 +0100

    Start fixing no userinfo

--- src/deserialize_uri.rs
+++ src/deserialize_uri.rs
@@ -1,6 +1,8 @@
 use http::Uri;
+use serde::de::Error;
 use serde::{de, Deserializer};
 use std::fmt;
+use std::fmt::Formatter;
 
 struct UriVisitor;
 impl<'de> de::Visitor<'de> for UriVisitor {
@@ -21,4 +23,37 @@ where
     D: Deserializer<'de>,
 {
     de.deserialize_str(UriVisitor)
+}
+
+struct OptUriVisitor;
+
+impl<'de> de::Visitor<'de> for OptUriVisitor {
+    type Value = Option<Uri>;
+
+    fn expecting(&self, formatter: &mut Formatter<'_>) -> fmt::Result {
+        write!(formatter, "valid or missing uri")
+    }
+
+    #[inline]
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        Ok(Some(UriVisitor.visit_str(v)?))
+    }
+
+    fn visit_none<E>(self) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        Ok(None)
+    }
+
+}
+
+pub fn deserialize_opt<'de, D>(de: D) -> Result<Option<Uri>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    de.deserialize_str(OptUriVisitor)
 }
